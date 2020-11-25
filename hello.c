a77cb86af565eca9383e49c64d2fddf2589acc66
Mayur Aitavadekar <mayur.a@innowise.us>
2020-11-25 22:47:56 +0530

    multiplication added

--- hello.c
+++ hello.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 
+void multiply(int a, int b) {
+    int c;
+    c = a * b;
+    printf("multiplication is = %d",c);
+}
+
 int main () {
     printf("hello world.");
+
+    multiply(5,2);
+
     return 0;
 }
