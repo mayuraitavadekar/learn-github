e63f53778f743a5380ef4f77bdfb27332c56f5c6
Mayur Aitavadekar <mayur.a@innowise.us>
2020-11-25 22:45:30 +0530

    subtraction is done

--- hello.c
+++ hello.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
+void subtract (int a, int b) {
+    int c;
+    c = a-b;
+    printf("subtraction is = %d", c);
+}
+
 int main () {
-    printf("hello world.");
+    printf("hello world. please do not add this line.");
+
+    subtract(20,30);
     return 0;
 }
